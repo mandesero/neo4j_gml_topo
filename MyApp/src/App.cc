94e60c8775d4f779447a8930c0f0411296252685
Maks <mt27@mail.ru>
2023-03-28 10:20:00 +0300

    app with time measurement

--- MyApp/src/App.cc
+++ MyApp/src/App.cc
@@ -1,5 +1,9 @@
 #include "App.hpp"
 #include <runos/core/logging.hpp>
+#include "SwitchManager.hpp"
+
+#include <chrono>
+
 
 namespace runos {
 
@@ -9,6 +13,16 @@ namespace runos {
         LOG(INFO) << "::: App initialization :::";
         topo = Topology::get(loader);
         connect(topo, &Topology::ready, this, &App::handle_signal);
+
+        auto sw_mgr = SwitchManager::get(loader);
+        // connect(sw_mgr, &SwitchManager::switchUp,
+        //         this, &App::handle_signal);
+        // connect(sw_mgr, &SwitchManager::switchDown,
+        //         this, &App::handle_signal);
+        // connect(sw_mgr, &SwitchManager::linkUp,
+        //         this, &App::handle_signal);
+        connect(sw_mgr, &SwitchManager::linkDown,
+                this, &App::handle_signal);
     }
     
     void App::startUp(Loader* loader) {
@@ -17,7 +31,15 @@ namespace runos {
 
     void App::handle_signal()  {
         LOG(INFO) << "::: ComputePath by App :::";
-        auto route_id = topo->newRoute(0, 2);
+        
+        std::chrono::high_resolution_clock::time_point start_time, end_time;
+        start_time = std::chrono::high_resolution_clock::now();
+
+        auto route_id = topo->newRoute(1, 2);
+
+        end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+        LOG(INFO) << "Find shortestPath time: " << duration.count() << " nanoseconds";
     }
 
 } // namespace runos
